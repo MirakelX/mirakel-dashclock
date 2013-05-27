db5ab3e202c371d5cfbbd0521fb8db30d52376b5
Georg Semmler <georg_semmler_05@web.de>
2013-05-27 14:24:17 +0200

    Fix Layout to Dashclocklayout Formate Source

--- src/de/azapps/mirakel/dashclock/MirakelExtension.java
+++ src/de/azapps/mirakel/dashclock/MirakelExtension.java
@@ -1,6 +1,5 @@
 package de.azapps.mirakel.dashclock;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,71 +23,75 @@ public class MirakelExtension extends DashClockExtension {
 	protected void onUpdateData(int reason) {
 		SharedPreferences prefs = PreferenceManager
 				.getDefaultSharedPreferences(this);
-		//Get values from Settings
+		// Get values from Settings
 		int list_id = Integer.parseInt(prefs.getString("startupList", "-1"));
-		int maxTasks=Integer.parseInt(prefs.getString("showTasks", "1"));
-		//Get where-clause
-		String where="";
-		if(list_id<0){
-			String[] col={"whereQuery"};
-			Cursor c=getContentResolver()
-					.query(Uri.parse("content://de.azapps.mirakel.provider/special_lists"),col," _id="+(-1*list_id),null,null);
-			if(c==null){
+		int maxTasks = Integer.parseInt(prefs.getString("showTasks", "1"));
+		// Get where-clause
+		String where = "";
+		if (list_id < 0) {
+			String[] col = { "whereQuery" };
+			Cursor c = getContentResolver()
+					.query(Uri
+							.parse("content://de.azapps.mirakel.provider/special_lists"),
+							col, " _id=" + (-1 * list_id), null, null);
+			if (c == null) {
 				Log.wtf(TAG, "Mirakel-Contentprovider not Found");
-				Toast.makeText(this, getString(R.string.installMirakel), Toast.LENGTH_SHORT).show();
+				Toast.makeText(this, getString(R.string.installMirakel),
+						Toast.LENGTH_SHORT).show();
 				return;
 			}
 			c.moveToFirst();
-			if(c.getCount()>0){
-				where=c.getString(0);
+			if (c.getCount() > 0) {
+				where = c.getString(0);
 			}
 			c.close();
 		}
-		//Get Tasks
+		// Get Tasks
 		String[] col = { "name,priority,due" };
 		Cursor c = getContentResolver()
 				.query(Uri.parse("content://de.azapps.mirakel.provider/tasks"),
 						col,
-						(where==""?" list_id=" + list_id :where)+ " and done=0 ",
+						(where == "" ? " list_id=" + list_id : where)
+								+ " and done=0 ",
 						null,
 						"priority desc, case when (due is NULL) then date('now','+1000 years') else date(due) end asc");
 		c.moveToFirst();
-		//Set Status
-		String status="";
-		if(c.getCount()==0)
-			status=getString(R.string.status0);
-		else if(c.getCount()==1)
-			status=getString(R.string.status1);
+		// Set Status
+		String status = "";
+		if (c.getCount() == 0)
+			status = getString(R.string.status0);
+		else if (c.getCount() == 1)
+			status = getString(R.string.status1);
 		else
-			status=c.getCount()+" "+getString(R.string.status2);
-		//Set Body
-		String expBody="";
-		if(c.getCount()>0){
-			SimpleDateFormat in=new SimpleDateFormat(getString(R.string.due_dbformat),Locale.US);
-			SimpleDateFormat out=new SimpleDateFormat(getString(R.string.due_outformat),Locale.getDefault());
-			int counter=0;
-			while (!c.isAfterLast()&&counter<maxTasks) {
+			status = c.getCount() + " " + getString(R.string.status2);
+		// Set Body
+		String expBody = "";
+		if (c.getCount() > 0) {
+			SimpleDateFormat in = new SimpleDateFormat(
+					getString(R.string.due_dbformat), Locale.US);
+			SimpleDateFormat out = new SimpleDateFormat(
+					getString(R.string.due_outformat), Locale.getDefault());
+			int counter = 0;
+			while (!c.isAfterLast() && counter < maxTasks) {
 				Date t = null;
 				try {
 					t = in.parse(c.getString(2));
 				} catch (ParseException e) {
 					Log.wtf(TAG, "failed to parse Date from db");
-				}
-				catch (NullPointerException e) {
+				} catch (NullPointerException e) {
 				}
 				expBody += c.getString(0)
 						+ (t == null ? " " : " " + getString(R.string.to) + " "
 								+ out.format(t));
 				c.moveToNext();
-				expBody+=(counter<maxTasks-1&&!c.isAfterLast()?"\n":"");
+				expBody += (counter < maxTasks - 1 && !c.isAfterLast() ? "\n"
+						: "");
 				++counter;
 			}
 		}
-		//Set Content
-		publishUpdate(new ExtensionData()
-				.visible(true)
-				.icon(R.drawable.ic_launcher)
-				.status(status)
+		// Set Content
+		publishUpdate(new ExtensionData().visible(true)
+				.icon(R.drawable.ic_launcher).status(status)
 				.expandedBody(expBody));
 
 	}
--- src/de/azapps/mirakel/dashclock/MirakelSettingsActivity.java
+++ src/de/azapps/mirakel/dashclock/MirakelSettingsActivity.java
@@ -1,59 +1,81 @@
-
 package de.azapps.mirakel.dashclock;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MirakelSettingsActivity extends PreferenceActivity {
 	private static final String TAG = "MirakelSettingsActivity";
 
-	@SuppressWarnings("deprecation")//TODO Why?
+	@SuppressWarnings("deprecation")
+	// TODO Why?
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getActionBar().setIcon(R.drawable.ic_launcher);
+		//Fix Layout
+		Drawable d = getResources().getDrawable(R.drawable.ic_launcher);
+		d.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+		getActionBar().setIcon(d);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		;
+		getActionBar().setBackgroundDrawable(
+				new ColorDrawable(Color.rgb(0, 153, 204)));
+		int titleId = Resources.getSystem().getIdentifier("action_bar_title",
+				"id", "android");
+		((TextView) findViewById(titleId)).setTextColor(Color.WHITE);
+		getListView().setDividerHeight(0);
 		addPreferencesFromResource(R.xml.pref_xml);
-		ListPreference startupListPreference =  (ListPreference) findPreference("startupList");
-		String[] s={"_id","name"};
-		//Get Lists from Mirakel-Contentresolver
-		Cursor c = getContentResolver().query(
-				Uri.parse("content://de.azapps.mirakel.provider/special_lists"),s ,
-				"1=1", null, null);
-		if(c==null){
+		ListPreference startupListPreference = (ListPreference) findPreference("startupList");
+
+		String[] s = { "_id", "name" };
+		// Get Lists from Mirakel-Contentresolver
+		Cursor c = getContentResolver()
+				.query(Uri
+						.parse("content://de.azapps.mirakel.provider/special_lists"),
+						s, "1=1", null, null);
+		if (c == null) {
 			Log.wtf(TAG, "Mirakel-Contentprovider not Found");
-			Toast.makeText(this, getString(R.string.installMirakel), Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, getString(R.string.installMirakel),
+					Toast.LENGTH_SHORT).show();
 			return;
 		}
-		List<CharSequence>values=new ArrayList<CharSequence>();
-		List<CharSequence>entries=new ArrayList<CharSequence>();
+		List<CharSequence> values = new ArrayList<CharSequence>();
+		List<CharSequence> entries = new ArrayList<CharSequence>();
 		c.moveToFirst();
-		while(!c.isAfterLast()){
-			values.add(""+(-1*c.getInt(0)));
+		while (!c.isAfterLast()) {
+			values.add("" + (-1 * c.getInt(0)));
 			entries.add(c.getString(1));
 			c.moveToNext();
 		}
 		c = getContentResolver().query(
-				Uri.parse("content://de.azapps.mirakel.provider/lists"),s ,
+				Uri.parse("content://de.azapps.mirakel.provider/lists"), s,
 				"1=1", null, "lft");
 		c.moveToFirst();
-		while(!c.isAfterLast()){
+		while (!c.isAfterLast()) {
 			values.add(c.getString(0));
 			entries.add(c.getString(1));
 			c.moveToNext();
-		}		
+		}
 		c.close();
-		startupListPreference.setEntries(entries.toArray(new String[entries.size()]));
-		startupListPreference.setEntryValues(values.toArray(new String[values.size()]));
-	}
+		startupListPreference.setEntries(entries.toArray(new String[entries
+				.size()]));
+		startupListPreference.setEntryValues(values.toArray(new String[values
+				.size()]));
 
+	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
